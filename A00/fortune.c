79471a7250d41f78e216a4c32b0b29ac6c22b3f3
Cecilia Chen <ceciliachen@wc131-dhcp-151.brynmawr.edu>
2022-09-01 12:46:26 -0400

    assignment 0 exercise

---
+++ A00/fortune.c
@@ -0,0 +1,13 @@
+/***************************************************
+ * hello.c
+ * Author: Yue Chen
+ * Date: 9/1/2022
+ * Print a fortune
+ */
+
+#include <stdio.h>
+
+int main() {
+  printf("A journey of a thousand miles begins with a single step\n");
+  return 0;
+}
